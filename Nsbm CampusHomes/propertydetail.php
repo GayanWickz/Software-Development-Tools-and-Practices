6c76b6e50334f4d35fc5002127ef211a13fe6925
Suresh Wickramarachchi <133583509+GayanWickz@users.noreply.github.com>
2024-03-13 00:17:17 +0530

    Property Details Page (details  dynamically displayed)
       1. Property detail page showcases all dynamic property information.
        2.Small map displays property location.
       3. Students can request reservations directly from landlords.
        4.Track reservation status and view landlord responses.
       5. Enhances user experience with real-time updates.
    6.The location of the property is displayed dynamically on the map.

---
+++ Nsbm CampusHomes/propertydetail.php
@@ -0,0 +1,288 @@
+<?php 
+ini_set('session.cache_limiter','public');
+session_cache_limiter(false);
+session_start();
+include("config.php");
+								
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Real Estate PHP">
+<meta name="keywords" content="">
+<meta name="author" content="Unicoder">
+<link rel="shortcut icon" href="images/favicon.ico">
+
+
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+<link rel="stylesheet" type="text/css" href="css/color.css" id="color-change">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+
+<title>NSBM CampusHomes</title>
+</head>
+<body>
+
+<?php include("include/header.php");?>
+
+
+<div id="page-wrapper">
+    <div class="row"> 
+       
+		
+       
+          <div class="full-row">
+            <div class="container">
+                <div class="row">
+				
+					<?php
+						$id=$_REQUEST['pid']; 
+						$query=mysqli_query($con,"SELECT property.*, user.* FROM `property`,`user` WHERE property.uid=user.uid and pid='$id'");
+						while($row=mysqli_fetch_array($query))
+						{
+					  ?>
+				  
+                    <div class="col-lg-8">
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div id="single-property" style="width:1000px; height:700px; margin:30px auto 50px;"> 
+                                    <!-- Slide 1-->
+                                    <div class="ls-slide" data-ls="duration:7500; transition2d:5; kenburnszoom:in; kenburnsscale:1.2;"> <img width="auto" height="auto" src="admin/property/<?php echo $row['11'];?>" class="ls-bg" alt="" /> </div>
+                                    
+                                    <!-- Slide 2-->
+                                    <div class="ls-slide" data-ls="duration:7500; transition2d:5; kenburnszoom:in; kenburnsscale:1.2;"> <img width="auto" height="auto" src="admin/property/<?php echo $row['12'];?>" class="ls-bg" alt="" /> </div>
+                                    
+                                    <!-- Slide 3-->
+                                    <div class="ls-slide" data-ls="duration:7500; transition2d:5; kenburnszoom:in; kenburnsscale:1.2;"> <img width="1920" height="1080" src="admin/property/<?php echo $row['13'];?>" class="ls-bg" alt="" /> </div>
+									
+									<!-- Slide 4-->
+									<div class="ls-slide" data-ls="duration:7500; transition2d:5; kenburnszoom:in; kenburnsscale:1.2;"> <img width="1920" height="1080" src="admin/property/<?php echo $row['14'];?>" class="ls-bg" alt="" /> </div>
+									
+									<!-- Slide 5-->
+									<div class="ls-slide" data-ls="duration:7500; transition2d:5; kenburnszoom:in; kenburnsscale:1.2;"> <img width="1920" height="1080" src="admin/property/<?php echo $row['15'];?>" class="ls-bg" alt="" /> </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mb-4">
+                            <div class="col-md-6">
+                                <div class="bg-success d-table px-3 py-2 rounded text-white text-capitalize">For <?php echo $row['4'];?></div>
+                                <h5 class="mt-2 text-secondary text-capitalize"><?php echo $row['1'];?></h5>
+                                <span class="mb-sm-20 d-block text-capitalize"><i class="fas fa-map-marker-alt text-success font-12"></i> &nbsp;<?php echo $row['10'];?></span>
+							</div>
+                            <div class="col-md-6">
+                                <div class="text-success text-left h5 my-2 text-md-right">$<?php echo $row['8'];?></div>
+                                <div class="text-left text-md-right">Price</div>
+                            </div>
+                        </div>
+                        <div class="property-details">
+                            <div class="bg-gray property-quantity px-4 pt-4 w-100">
+                                <ul>
+                                    
+                                    <li><span class="text-secondary"><?php echo $row['6'];?></span> Bedroom</li>
+                                    <li><span class="text-secondary"><?php echo $row['7'];?></span> Bathroom</li>
+                                    <li><span class="text-secondary"><?php echo $row['8'];?></span> Balcony</li>
+                                    <li><span class="text-secondary"><?php echo $row['10'];?></span> Hall</li>
+                                    <li><span class="text-secondary"><?php echo $row['9'];?></span> Kitchen</li>
+                                </ul>
+                            </div>
+                            <h4 class="text-secondary my-4">Description</h4>
+                            <p><?php echo $row['2'];?></p>
+                            
+                            <h5 class="mt-5 mb-4 text-secondary">Property Summary</h5>
+                            <div  class="table-striped font-14 pb-2">
+                                <table class="w-100">
+                                    <tbody>
+                                        <tr>
+                                            <td>Posted on :</td>
+                                            <td class="text-capitalize"><?php echo $row['19'];?></td>
+                                            <td>Property Type :</td>
+                                            <td class="text-capitalize"><?php echo $row['3'];?></td>
+                                        </tr>
+                                        
+                                        <tr>
+                                            <td>Address :</td>
+                                            <td class="text-capitalize"><?php echo $row['9'];?></td>
+                                            <td>Status :</td>
+                                            <td class="text-capitalize"><?php echo $row['17'];?></td>
+                                        </tr>
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
+                            <h5 class="mt-5 mb-4 text-secondary">To request your reservation, please click here</h5>
+                            <div class="row">
+								
+								
+                                <!-- Reservation Button -->
+                                <div class="mt-4">
+                               <a id="reservationButton" class="btn btn-success" href="reserve-property.php?pid=<?php echo $row['0']; ?>">Reserve Property</a>
+                                 </div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+                                 
+                                 <div class="mt-4">
+                                    Reservation Status: 
+                                 <span id="reservationStatus">Checking reservation status...</span>
+                                </div>
+
+
+                            </div>   
+							
+
+                            <h5 class="mt-5 mb-4 text-secondary double-down-line-left position-relative">Contact Agent</h5>
+                            <div class="agent-contact pt-60">
+                                <div class="row">
+                                    <div class="col-sm-4 col-lg-3"> <img src="admin/user/<?php echo $row['uimage']; ?>" alt="" height="200" width="170"> </div>
+                                    <div class="col-sm-8 col-lg-9">
+                                        <div class="agent-data text-ordinary mt-sm-20">
+                                            <h6 class="text-success text-capitalize"><?php echo $row['uname'];?></h6>
+                                            <ul class="mb-3">
+                                                <li><?php echo $row['uphone'];?></li>
+                                                <li><?php echo $row['uemail'];?></li>
+                                            </ul>
+                                            
+                                            <div class="mt-3 text-secondary hover-text-success">
+                                                <ul>
+                                                    <li class="float-left mr-3"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                                    <li class="float-left mr-3"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                                    <li class="float-left mr-3"><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                                                    <li class="float-left mr-3"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                                    <li class="float-left mr-3"><a href="#"><i class="fas fa-rss"></i></a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+					
+					<?php } ?>
+					
+                    <div class="col-lg-4">
+                        
+                    <h4 class="double-down-line-left text-secondary position-relative pb-4 my-4">Location of the Property</h4>
+                     
+        <div>
+        <?php
+
+
+
+
+$id = $_GET['pid']; 
+$_SESSION['pid'] = $id; 
+
+
+// Include the file
+include "map for details page/com.php";
+?>
+
+                        </div>
+                        <h4 class="double-down-line-left text-secondary position-relative pb-4 mb-4 mt-5">Featured Property</h4>
+                        <ul class="property_list_widget">
+							
+                            <?php 
+                            $query=mysqli_query($con,"SELECT * FROM `property` WHERE isFeatured = 1 ORDER BY date DESC LIMIT 3");
+                                    while($row=mysqli_fetch_array($query))
+                                    {
+                            ?>
+                            <li> <img src="admin/property/<?php echo $row['18'];?>" alt="pimage">
+                                <h6 class="text-secondary hover-text-success text-capitalize"><a href="propertydetail.php?pid=<?php echo $row['0'];?>"><?php echo $row['1'];?></a></h6>
+                                <span class="font-14"><i class="fas fa-map-marker-alt icon-success icon-small"></i> <?php echo $row['14'];?></span>
+                                
+                            </li>
+                            <?php } ?>
+
+                        </ul>
+
+                        <div class="sidebar-widget mt-5">
+                            <h4 class="double-down-line-left text-secondary position-relative pb-4 mb-4">Recently Added Property</h4>
+                            <ul class="property_list_widget">
+							
+								<?php 
+								$query=mysqli_query($con,"SELECT * FROM `property` ORDER BY date DESC LIMIT 7");
+										while($row=mysqli_fetch_array($query))
+										{
+								?>
+                                <li> <img src="admin/property/<?php echo $row['18'];?>" alt="pimage">
+                                    <h6 class="text-secondary hover-text-success text-capitalize"><a href="propertydetail.php?pid=<?php echo $row['0'];?>"><?php echo $row['1'];?></a></h6>
+                                    <span class="font-14"><i class="fas fa-map-marker-alt icon-success icon-small"></i> <?php echo $row['14'];?></span>
+                                    
+                                </li>
+                                <?php } ?>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+		
+        
+        
+        <!-- Scroll to top --> 
+        <a href="#" class="bg-secondary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
+        
+    </div>
+</div>
+
+<script>
+    // Retrieve the property ID from the PHP variable directly
+    var propertyId = <?php echo $_GET['pid'] ?? 'null'; ?>;
+    
+    // Fetch the reservation status from the server
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'reservation-status.php?pid=' + propertyId);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            // Update the reservation status text based on the response
+            document.getElementById('reservationStatus').innerText = xhr.responseText;
+        } else {
+           
+            document.getElementById('reservationStatus').innerText = 'Failed to fetch reservation status.';
+        }
+    };
+    xhr.send();
+</script>
+<!--	Js Link--> 
+
+<script src="js/jquery.min.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/greensock.js"></script> 
+<script src="js/layerslider.transitions.js"></script> 
+<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
+<!--jQuery Layer Slider --> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/tmpl.js"></script> 
+<script src="js/jquery.dependClass-0.1.js"></script> 
+<script src="js/draggable-0.1.js"></script> 
+<script src="js/jquery.slider.js"></script> 
+<script src="js/wow.js"></script> 
+<script src="js/custom.js"></script> 
+
+</body>
+<?php include("footer.php");?>
+</html>
