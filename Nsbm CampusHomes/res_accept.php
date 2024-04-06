b159929df0b679c3f06861af50af2110d2afbfe9
Suresh Wickramarachchi <133583509+GayanWickz@users.noreply.github.com>
2024-04-06 16:13:02 +0530

    Backend for Student Property Request

---
+++ Nsbm CampusHomes/res_accept.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include("config.php");
+
+if(isset($_SESSION['uemail'])) {
+    if(isset($_GET['id'])) {
+        $id = mysqli_real_escape_string($con, $_GET['id']);
+        
+       
+        $Query = "UPDATE reservation_requests SET status = 'accepted' WHERE id = '$id'";
+        if(mysqli_query($con, $Query)) {
+            header("Location: user-listed-property.php?msg=Reservation request accepted successfully.");
+            header("Location: reservation_accept_reject.php");
+            exit();
+           
+        } else {
+            echo "Error updating status: " . mysqli_error($con);
+        }
+    } else {
+        echo "Error: ID not provided.";
+    }
+} else {
+    echo "Error: User not logged in.";
+}
+?>
