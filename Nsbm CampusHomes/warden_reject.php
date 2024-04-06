5121384547dc734df0b297105afd96fc7323ff24
Suresh Wickramarachchi <133583509+GayanWickz@users.noreply.github.com>
2024-04-07 00:37:33 +0530

    Warden Property Accept and Reject Feature

---
+++ Nsbm CampusHomes/warden_reject.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include("config.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["reject"])) {
+    
+    $pid = $_POST["pid"];
+    
+    // if warden provide ,
+    $reason = isset($_POST["reason"]) ? $_POST["reason"] : '';
+
+   
+    $query = "UPDATE property SET approval_status = 'rejected'";
+    if (!empty($reason)) {
+        
+        $query .= ", approval_reason = '$reason'";
+    }
+    $query .= " WHERE pid = $pid";
+
+    // Execute the update query
+    if (mysqli_query($con, $query)) {
+        
+        header("Location: " . $_SERVER["HTTP_REFERER"]); // Redirect back to the previous page
+        exit(); 
+    } else {
+    
+        echo "Error updating record: " . mysqli_error($con);
+    }
+} else {
+    
+    echo "Invalid request!";
+}
+?>
