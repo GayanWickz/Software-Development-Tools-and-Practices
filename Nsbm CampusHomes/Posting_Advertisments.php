1c479ba45c1567997c95f766f125ec8cdd2f3420
D-Thanura <149312340+D-Thanura@users.noreply.github.com>
2024-04-06 11:06:09 +0530

    Add Admin Advertisment Feature with backend

---
+++ Nsbm CampusHomes/Posting_Advertisments.php
@@ -0,0 +1,4 @@
+<form action="advertisment_upload.php" method="post" enctype="multipart/form-data">
+  <input type="file" name="file[]" multiple accept="image/*">
+  <input type="submit" value="Upload">
+</form>
